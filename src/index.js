1303c29bf4d8bbf785a5d498a08112c0a899c44f
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-05-07 19:31:39 -0300

    Retirando tentativas do bonus

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import "./css/reset.css"
 import "./css/style.css"
 import Topo from "./componentes/Topo"
 import Cardapio from "./componentes/Cardapio"
-import ConfirmarPedido from "./componentes/ConfirmarPedido"
 
  export default function App() {
     const [comboEscolhido, setComboEscolhido] = React.useState([{id: 0, data:[]},{id: 1, data:[]},{id: 2, data:[]}]);
     return (
       <div className="corpo-total">
-        <ConfirmarPedido />
         <div className="antes-confirmar">
           <Topo />
           <Cardapio comboEscolhido={comboEscolhido} setComboEscolhido={setComboEscolhido}/>
